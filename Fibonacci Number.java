89a85aa8b4272c563d80dde3885de7112a4810ba
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-14 23:00:37 +0900

    Update Fibonacci Number.java

--- Fibonacci Number.java
+++ Fibonacci Number.java
@@ -15,3 +15,12 @@ class Solution {
 }
 // Runtime: 21 ms, faster than 59.52% of Java online submissions for First Unique Character in a String.
 // Memory Usage: 39.7 MB, less than 43.02% of Java online submissions for First Unique Character in a String.
+
+public class Solution {
+    public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        return fib(N - 1) + fib(N - 2);
+    }
+}

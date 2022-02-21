d7c4cf8811cd04a75af8829ca5b874d3e6df85be
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-02-21 21:29:25 +0900

    Update Remove Duplicates from Sorted Array.java

--- Remove Duplicates from Sorted Array.java
+++ Remove Duplicates from Sorted Array.java
@@ -14,3 +14,18 @@ class Solution {
 }
 // Runtime: 1 ms, faster than 88.75% of Java online submissions for Remove Duplicates from Sorted Array.
 // Memory Usage: 47.9 MB, less than 8.75% of Java online submissions for Remove Duplicates from Sorted Array.
+
+class Solution{
+public int removeDuplicates(int[] nums) {
+		int length = nums.length;
+		for(int i=length-2;i>=0;i--) {
+			if(nums[i] == nums[i+1]) {
+				for(int j=i+1;j<length;j++) {
+					nums[j-1] = nums[j];
+				}
+				length--;
+			}
+		}
+		return length;
+	}
+}

b1b59f13aed54b2232b1a3188ea20a1818c4cb00
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-02-26 14:07:03 +0900

    Update Two Sum II - Input Array Is Sorted.java

--- Two Sum II - Input Array Is Sorted.java
+++ Two Sum II - Input Array Is Sorted.java
@@ -13,3 +13,23 @@ class Solution {
 }
 // Runtime: 407 ms, faster than 5.01% of Java online submissions for Two Sum II - Input Array Is Sorted.
 // Memory Usage: 44.6 MB, less than 37.33% of Java online submissions for Two Sum II - Input Array Is Sorted.
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int left=0,right=n-1;
+        while(left<right){
+            int sum = numbers[left]+numbers[right];
+            if(sum==target){
+                return new int[]{left+1,right+1};
+            }else if(sum>target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+// Runtime: 2 ms, faster than 46.27% of Java online submissions for Two Sum II - Input Array Is Sorted.
+// Memory Usage: 50.1 MB, less than 13.17% of Java online submissions for Two Sum II - Input Array Is Sorted.

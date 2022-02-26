e7a25daab3b832f3e3f00466fcd36a0c23e1ee7b
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-02-26 10:51:20 +0900

    Update Rotate Array.java

--- Rotate Array.java
+++ Rotate Array.java
@@ -13,3 +13,20 @@ class Solution {
     }
 }
 // Time Limit Exceeded.
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] tempNums = new int[n];
+        for(int i=0;i<n;i++){
+            //  [1,2,3,4,5,6,7],k=3の場合、[(i+k) % n]は3,4,5,6,0,1,2になる
+            tempNums[(i+k) % n] = nums[i];
+            System.out.println((i+k) % n);
+        }
+        for(int i=0;i<n;i++){
+            nums[i] = tempNums[i];
+        }
+    }
+}
+// Runtime: 1 ms, faster than 82.75% of Java online submissions for Rotate Array.
+// Memory Usage: 57.6 MB, less than 87.66% of Java online submissions for Rotate Array.

7b3ded51260d230869dcacdc91a8e0f5dcad9c22
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-02 08:58:24 +0900

    Update rangeSumBST.java

--- rangeSumBST.java
+++ rangeSumBST.java
@@ -44,6 +44,37 @@ class Solution {
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Range Sum of BST.
 // Memory Usage: 49.4 MB, less than 87.90% of Java online submissions for Range Sum of BST.
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans=0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        dfs(root,low,high);
+        return ans;
+    }
+    public void dfs(TreeNode root, int low, int high){
+        if(root != null){
+            if(low <= root.val && root.val <= high) ans+= root.val;
+            if(low < root.val) dfs(root.left,low,high);
+            if(root.val < high) dfs(root.right,low,high);
+        }
+    }
+}
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Range Sum of BST.
+// Memory Usage: 67.9 MB, less than 7.83% of Java online submissions for Range Sum of BST.
 
 /**
  * Definition for a binary tree node.

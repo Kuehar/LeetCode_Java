317388cdeacea7b188c4bd080555759a88ed9406
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-07 22:26:04 +0900

    Update Reverse Linked List.java

--- Reverse Linked List.java
+++ Reverse Linked List.java
@@ -23,3 +23,25 @@ class Solution {
 }
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
 // Memory Usage: 38.5 MB, less than 89.03% of Java online submissions for Reverse Linked List.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+// Memory Usage: 43.2 MB, less than 17.59% of Java online submissions for Reverse Linked List.

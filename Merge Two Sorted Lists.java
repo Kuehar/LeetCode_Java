da0dae5294c6bcb3eb0deb5475dc5a3ce23098d0
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-11 23:56:48 +0900

    Update Merge Two Sorted Lists.java

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// Iterative
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
@@ -30,3 +31,34 @@ class Solution {
 }
 // Runtime: 1 ms, faster than 72.74% of Java online submissions for Merge Two Sorted Lists.
 // Memory Usage: 43.3 MB, less than 21.92% of Java online submissions for Merge Two Sorted Lists.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// Recursive
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        
+        else if(list2 == null){
+            return list1; 
+        }
+        
+        else if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
+        }else{
+            list2.next = mergeTwoLists(list1,list2.next);
+            return list2;
+        }
+    }
+}

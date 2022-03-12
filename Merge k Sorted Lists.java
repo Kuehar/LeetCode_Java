37e7a6b31634f01103969d74527fad57686cd7f5
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-12 22:06:43 +0900

    Update Merge k Sorted Lists.java

--- Merge k Sorted Lists.java
+++ Merge k Sorted Lists.java
@@ -35,3 +35,46 @@ class Solution {
 }
 // Runtime: 10 ms, faster than 37.80% of Java online submissions for Merge k Sorted Lists. O(NlogN).
 // Memory Usage: 48.7 MB, less than 5.48% of Java online submissions for Merge k Sorted Lists.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// 値を比較する解法
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int min_index = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while(true){
+            boolean isBreak = true;
+            int min = Integer.MAX_VALUE;
+            for(int i=0;i<lists.length;i++){
+                if(lists[i] != null){
+                    if(lists[i].val < min){
+                        min_index = i;
+                        min = lists[i].val;
+                    }
+                    isBreak = false;
+                }
+            }
+            if(isBreak){
+                break;
+            }
+            ListNode a = new ListNode(lists[min_index].val);
+            current.next = a;
+            current = current.next;
+            lists[min_index] = lists[min_index].next;
+        }
+        current.next = null;
+        return dummy.next;
+    }
+}
+// Runtime: 413 ms, faster than 5.03% of Java online submissions for Merge k Sorted Lists. O(n)
+// Memory Usage: 48.2 MB, less than 9.49% of Java online submissions for Merge k Sorted Lists.

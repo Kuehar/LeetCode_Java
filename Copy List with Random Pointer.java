f019c07ca09680c37529965fab1893297e1362af
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-16 13:18:28 +0900

    Create Copy List with Random Pointer.java

---
+++ Copy List with Random Pointer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Set<Integer> maximums = new HashSet<Integer>();
+        for(int n:nums){
+            maximums.add(n);
+            if(maximums.size() > 3){
+                maximums.remove(Collections.min(maximums));
+            }
+        }
+        if(maximums.size() == 3) return Collections.min(maximums);
+        return Collections.max(maximums);
+    }
+}

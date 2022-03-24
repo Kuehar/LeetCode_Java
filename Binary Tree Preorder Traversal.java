177e3963a19ec5625434406916a583e58ed397b0
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-24 13:09:04 +0900

    Update Binary Tree Preorder Traversal.java

--- Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal.java
@@ -39,3 +39,31 @@ class Solution {
         return ans;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> ans = new LinkedList<>();
+        if(root != null){
+            ans.add(root.val);
+            ans.addAll(preorderTraversal(root.left));
+            ans.addAll(preorderTraversal(root.right));
+        }
+        return ans;
+    }
+}

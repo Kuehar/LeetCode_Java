fbc6866da9c1ca442c56f0524e195549a69871bd
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-03-25 22:14:21 +0900

    Update Binary Tree Inorder Traversal.java

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -30,3 +30,32 @@ class Solution {
 }
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Inorder Traversal.
 // Memory Usage: 42.1 MB, less than 32.40% of Java online submissions for Binary Tree Inorder Traversal.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+    LinkedList<Integer> ans = new LinkedList<>();
+        if(root != null){
+            ans.addAll(inorderTraversal(root.left));
+            ans.add(root.val);
+            ans.addAll(inorderTraversal(root.right));
+        }
+        return ans;       
+    }
+}
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Inorder Traversal.
+// Memory Usage: 40.1 MB, less than 89.88% of Java online submissions for Binary Tree Inorder Traversal.

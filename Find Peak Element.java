6159378f3ba4c04d7120ab824437f50869226c06
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-04-29 23:43:43 +0900

    Update Find Peak Element.java

--- Find Peak Element.java
+++ Find Peak Element.java
@@ -13,3 +13,20 @@ public class Solution {
 }
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Peak Element.
 // Memory Usage: 41.5 MB, less than 94.72% of Java online submissions for Find Peak Element.
+
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] > nums[mid+1]){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Peak Element.
+// Memory Usage: 43.3 MB, less than 19.23% of Java online submissions for Find Peak Element.

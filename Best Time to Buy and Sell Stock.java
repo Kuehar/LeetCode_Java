a12fa73737a5e55c4c3439543464ca4ab8cd3049
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-05-07 23:45:53 +0900

    Update Best Time to Buy and Sell Stock.java

--- Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock.java
@@ -9,6 +9,7 @@ class Solution {
         return ans;
     }
 }
+// Time complexity is O(N).
 // Runtime: 15 ms, faster than 5.24% of Java online submissions for Best Time to Buy and Sell Stock.
 // Memory Usage: 90.4 MB, less than 37.45% of Java online submissions for Best Time to Buy and Sell Stock.
 

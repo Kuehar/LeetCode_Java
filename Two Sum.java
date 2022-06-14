17bbf3e57c6d41e13f7c205b048ad55b27b736d6
Kuehar <50407759+Kuehar@users.noreply.github.com>
2022-06-14 23:27:57 +0900

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -10,5 +10,21 @@ class Solution {
         return null;
     }
 }
-// Runtime: 133 ms, faster than 6.72% of Java online submissions for Two Sum.
+// Runtime: 133 ms, faster than 6.72% of Java online submissions for Two Sum. O(N^2)
 // Memory Usage: 44.8 MB, less than 64.49% of Java online submissions for Two Sum.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
+// Runtime: 2 ms, faster than 91.18% of Java online submissions for Two Sum. O(N)
+// Memory Usage: 42.1 MB, less than 96.20% of Java online submissions for Two Sum.
